524ce5d0be8029c447b6bf926644e62d7d878059
vamshidharparepalli <38714247+vamshidharparepalli@users.noreply.github.com>
2022-06-13 14:18:39 +0530

    Code to search an address and find compatibility
    This code is a positive test scenario only. 
    This code was generated to check if the address has NBN compatibility.

--- Belong.cy.js
+++ Belong.cy.js
@@ -6,13 +6,13 @@ describe('Belong Internet Connection', () => {
       cy.visit("https://www.belong.com.au/");
       //Asserts the URl.
       cy.url().should("include", "belong");
-      cy.wait(5000);
+      cy.wait(1000);
   });
   
   it("clicks the See internet Plans", () => {
     //clicks on the See internet plans button. 
       cy.get(':nth-child(3) > [data-testid="action-tile__root"] > .ActionTilestyles__ContentWrapper-y8oq8z-5 > [data-testid="action-tile__button"]').click();
-        cy.wait(5000);
+        cy.wait(1000);
     //The following syntax will help us to know if the User has been navigated to the Internet plans page.
       cy.url().should("include","internet");
   });
@@ -20,26 +20,25 @@ describe('Belong Internet Connection', () => {
   it("clicks on check your address", () => {
     //Clicks on the Check your address button
     cy.get('[data-testid="primary-button"]').click()
-    cy.wait(5000);
+    cy.wait(1000);
     //This syntax will help us to know if the user is navigated to the address check page. 
     cy.url().should("include","check-your-address");
 });
 
-  it("should search for your address Unit 1 25 Thomson Street, MAIDSTONE  VIC  3012", () => {
+  it("should search for your address Unit 25  6-8 Thomson Street, TWEED HEADS  NSW  2485", () => {
   //Helps input the address which the user is trying to search for. 
-  cy.get('[id="queryInput"]').type("Unit 1 25 Thomson street");
+  cy.get('[id="queryInput"]').type("Unit 25  6-8 Thomson Street");
+  cy.log();
   cy.wait(5000);
-  //Helps in indentifying all the similar addresses from the drop down and assign the vaules to a Jquery element which acts as a variable (address)
-  cy.get("ul[role='listbox']").each(function(address,index,$list){
-    //If statement to select the needed address by the user else to just the print log
-  if (address.text().includes("Unit 1 25 Thomson Street, MAIDSTONE VIC 3012")) 
-  {
- cy.wrap(address).click()
-  }
-  else 
-  {
-  cy.log(address.text() )
-  }  
-  })
+  cy.get('#queryList>li:nth-child(1)').click();
+  cy.wait(5000);
+});
+
+it("should check the address and confirm the connectivity ", () => {
+  //Helps input the address which the user is trying to search for. 
+  cy.get('.ButtonGroup__ButtonWrapper-ozgsdj-0>[data-testid="step-address-lookup__submit-button"]').click();
+  cy.wait(6000);
+// this helps to assert if the user's address is eligible to the NBN Connection
+  cy.get('.InnerSection-tykyg0-0 > .Heading-sc-1vh9mjn-0').contains('Great! We can connect you to NBN internet')
 });
 });
